b71ec45cde0d02f6bd822e473da6d29a27f63e96
cyansimireallen-ops <cyansimireallen07@gmail.com>
2026-09-10 14:12:00 -0700

    Update DashboardLayout.jsx

--- client/src/components/DashboardLayout.jsx
+++ client/src/components/DashboardLayout.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from './Navbar';
 import DashboardSidebar from './DashboardSidebar';
+import DashboardMobileTabs from './DashboardMobileTabs';
 
 export default function DashboardLayout() {
   return (
@@ -9,6 +10,7 @@ export default function DashboardLayout() {
       <div className="mx-auto flex w-full max-w-7xl flex-1">
         <DashboardSidebar />
         <main className="flex-1 p-4 sm:p-6 lg:p-8">
+          <DashboardMobileTabs />
           <Outlet />
         </main>
       </div>
