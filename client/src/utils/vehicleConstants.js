ec8f842a1c54f68eb2c71d217c32d6bacdbdb1c8
igabe-ops <igabenadine021507@example.com>
2026-09-19 09:05:58 -0700

    Add project files

--- client/src/utils/vehicleConstants.js
+++
@@ -1,9 +0,0 @@
-export const VEHICLE_TYPES = ['car', 'motorcycle', 'truck', 'bus', 'van', 'other'];
-export const TRANSMISSIONS = ['automatic', 'manual'];
-export const FUEL_TYPES = ['petrol', 'diesel', 'electric', 'hybrid'];
-export const CONDITIONS = ['new', 'used'];
-
-export const VEHICLE_FEATURES = [
-  'Air Conditioning', 'Bluetooth', 'Backup Camera', 'Leather Seats',
-  'Sunroof', 'Navigation', 'ABS', 'Alloy Wheels', 'USB Charger',
-];
